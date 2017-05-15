d372c3b22cc6259c11bc3ee6ea4aef0ece9b044a
hilaldonmez <hilaldnmez@yahoo.com>
2017-05-15 23:34:47 +0300

    Comments were added for test functions

--- ConcerterService/src/test/java/com/boun/nine/service/test/UserResourceTest.java
+++ ConcerterService/src/test/java/com/boun/nine/service/test/UserResourceTest.java
@@ -17,6 +17,15 @@ public class UserResourceTest extends JerseyTest{
 	protected Application configure(){
 		return new ResourceConfig(UserResource.class);
 	}
+	
+	/**
+	 * Create a user instance whose email is hilaldnmz@gmail.com and password is 1a in database with sample.sql
+	 * Check whether this instance can be created with login function
+	 * Check whether the expected response and the response obtained are the same
+	 * @return the successful result if they are the same 
+	 * @return error if they are not equal  
+	 */
+	
 	@Test
 	public void loginTest(){
 		String response = target("user/\"hilaldnmz@gmail.com\"/\"1a\"").request().get(String.class);
@@ -25,6 +34,13 @@ public class UserResourceTest extends JerseyTest{
 				+ "\"email\":\"hilaldnmz@gmail.com\",\"password\":\"1a\"}";
 		Assert.assertTrue(expected.equals(response));
 	}
+	
+	/**
+	 * Create a user instance whose name is hilal, username is hilaldnmz,email is hilaldonmez@gmail.com, and password is aaaa
+	 * @return the successful result if HTTP 200 is obtained as a respponse
+	 * @return error otherwise
+	 */
+	
 	@Test
 	public void signUpTest(){
 		Response response = target("user").request().post(Entity.json("{\"name\":\"hilal\",\"username\":\"hilaldnmz\",\"email\":\"hilaldonmez@gmail.com\","
