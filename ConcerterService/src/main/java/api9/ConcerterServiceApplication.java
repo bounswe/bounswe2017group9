fe91b98eef41d8fafb561087dde7ff806a8c3047
Fatih Güven <fatih.guven@boun.edu.tr>
2017-05-16 00:44:47 +0300

    Javadocs added

--- ConcerterService/src/main/java/api9/ConcerterServiceApplication.java
+++ ConcerterService/src/main/java/api9/ConcerterServiceApplication.java
@@ -2,6 +2,13 @@ package api9;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
+/**
+ * Creates a dynamic web application with default JAX-RS template.
+ * @author ffguven
+ * @version 1.0
+ * @since 15-05-2017
+ * 
+ */
 public class ConcerterServiceApplication extends ResourceConfig{
 	public ConcerterServiceApplication(){
 		packages("com.boun.nine.service.concrete");
