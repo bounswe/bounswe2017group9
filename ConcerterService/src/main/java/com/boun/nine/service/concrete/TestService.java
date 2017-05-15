044133f7ef5318c2599a06842f532f36cce618b2
Fatih Güven <fatih.guven@boun.edu.tr>
2017-05-16 00:43:33 +0300

    Javadocs added

--- ConcerterService/src/main/java/com/boun/nine/service/concrete/TestService.java
+++ ConcerterService/src/main/java/com/boun/nine/service/concrete/TestService.java
@@ -4,8 +4,22 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+/**
+ * 
+ * @author ffguven
+ * @version 1.0
+ * @since 15/05/2017
+ * This class is triggered if
+ * {@code ConcerterService/rest/test} URL is requested with GET.
+ * 
+ */
 @Path("/test")
 public class TestService {
+	/**
+	 * This function is triggered if {@code ConcerterService/rest/test/check}.
+	 * The URL stands for checking whether the API works or not.
+	 * @return String
+	 */
 	@GET
 	@Path("check")
 	@Produces("text/plain")
