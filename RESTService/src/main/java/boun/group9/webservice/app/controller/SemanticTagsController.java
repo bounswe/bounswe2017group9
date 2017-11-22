e3b8bf44fb7df01d826440bb6f99e63c14c93096
kasimbozdag <bozdag80@yahoo.com>
2017-11-22 23:25:19 +0200

    Creating ConcertTags Class

--- RESTService/src/main/java/boun/group9/webservice/app/controller/SemanticTagsController.java
+++ RESTService/src/main/java/boun/group9/webservice/app/controller/SemanticTagsController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.google.gson.JsonSyntaxException;
 
 import boun.group9.webservice.app.Application;
+import boun.group9.webservice.app.data.ConcertTags;
 import boun.group9.webservice.app.data.Concerts;
 import boun.group9.webservice.app.data.SemanticTags;
 import boun.group9.webservice.app.data.Users;
@@ -26,14 +27,14 @@ import boun.group9.webservice.helper.UserChecker;
 
 @RestController
 public class SemanticTagsController {
-	@RequestMapping(value="new-sementictag",method=RequestMethod.POST)
-	public String newSementicTag(@RequestBody String body) {
+	@RequestMapping(value="new-semantictag",method=RequestMethod.POST)
+	public String newsemanticTag(@RequestBody String body) {
 		SemanticTags tag;
 		String query;
 		ResultSet rs;
 		try {
 			tag = Application.gson.fromJson(body, SemanticTags.class);
-			query = SemanticTagsChecker.insertSementicTagsQuery(tag);
+			query = SemanticTagsChecker.insertSemanticTagsQuery(tag);
 			System.out.println(query);
 			rs = Database.connect(query,Application.MODE_UPDATE);
 			return "OK.";
@@ -48,10 +49,32 @@ public class SemanticTagsController {
 			throw new InternalServerException();
 		}
 	}
-	@RequestMapping(value="sementictags",method=RequestMethod.GET)
-	public String getSementicTags() {
+	@RequestMapping(value="new-concerttag",method=RequestMethod.POST)
+	public String newConcertTag(@RequestBody String body) {
+		ConcertTags ctag;
+		String query;
+		ResultSet rs;
+		try {
+			ctag = Application.gson.fromJson(body, ConcertTags.class);
+			query = SemanticTagsChecker.insertConcertTagsQuery(ctag);
+			System.out.println(query);
+			rs = Database.connect(query,Application.MODE_UPDATE);
+			return "OK.";
+		}catch(JsonSyntaxException ex) {
+			ex.printStackTrace();
+			throw new IJsonSyntaxException();
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+			throw new ISQLException();
+		}catch(NotSavedException ex) {
+			ex.printStackTrace();
+			throw new InternalServerException();
+		}
+	}
+	@RequestMapping(value="semantictags",method=RequestMethod.GET)
+	public String getsemanticTags() {
 		String jsonString="";
-		String query="SELECT * FROM SementicTags;";
+		String query="SELECT * FROM semanticTags;";
 		System.out.println(query);
 		ResultSet rs;
 		SemanticTags tag;
@@ -77,10 +100,10 @@ public class SemanticTagsController {
 		}
 		return jsonString;
 	}
-	@RequestMapping(value="sementictags/{tagID}",method=RequestMethod.GET)
-	public String getSementicTag(@PathVariable(value="tagID") String tagID) {
+	@RequestMapping(value="semantictags/{tagID}",method=RequestMethod.GET)
+	public String getsemanticTag(@PathVariable(value="tagID") String tagID) {
 		String jsonString="";
-		String query="SELECT * FROM SementicTags WHERE id=\""+tagID+"\";";
+		String query="SELECT * FROM semanticTags WHERE id=\""+tagID+"\";";
 		System.out.println(query);
 		ResultSet rs;
 		SemanticTags tag;
@@ -91,8 +114,38 @@ public class SemanticTagsController {
 				tag.setId(rs.getString("id"));
 				tag.setLabel(rs.getString("label"));
 				tag.setSearch(rs.getString("search"));
+				tag.setDescription(rs.getString("description"));
 				jsonString = Application.gson.toJson(tag);
 			}
+		}catch(SQLException ex) {
+			System.out.println("SQL Exception occured");
+			ex.printStackTrace();
+			return "SQL Error occured.";
+		}catch(NotSavedException ex) {
+			ex.printStackTrace();
+			return "Not saved.";
+		}
+		return jsonString;
+	}
+	@RequestMapping(value="concerttags/{concertID}",method=RequestMethod.GET)
+	public String getConcertTags(@PathVariable(value="concertID") int concertID) {
+		String jsonString="";
+		String query="SELECT * FROM ConcertTags WHERE concert_id="+concertID+";";
+		System.out.println(query);
+		ResultSet rs;
+		ConcertTags ctag;
+		ArrayList<ConcertTags> ctagList = new ArrayList<ConcertTags>();
+		try {
+			rs = Database.connect(query, Application.MODE_GET);
+			while(rs.next()) {
+				ctag = new ConcertTags();
+				ctag.setId(rs.getInt("id"));
+				ctag.setTag_id(rs.getString("tag_id"));
+				ctag.setConcert_id(rs.getInt("concert_id"));
+				ctag.setCreated_at(rs.getDate("created_at"));
+				ctagList.add(ctag);
+			}
+			jsonString = Application.gson.toJson(ctagList);
 		}catch(SQLException ex) {
 			System.out.println("SQL Exception occured");
 			ex.printStackTrace();
