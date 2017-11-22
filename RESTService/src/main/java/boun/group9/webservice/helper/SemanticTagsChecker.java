6197e0b1ae43859eeddd57f803334ef42a224f1d
kasimbozdag <bozdag80@yahoo.com>
2017-11-22 22:11:12 +0200

    Creating SemanticTags Class

--- RESTService/src/main/java/boun/group9/webservice/helper/SemanticTagsChecker.java
+++ RESTService/src/main/java/boun/group9/webservice/helper/SemanticTagsChecker.java
@@ -5,7 +5,7 @@ import boun.group9.webservice.app.data.SemanticTags;
 public class SemanticTagsChecker {
 	public static String insertSementicTagsQuery(SemanticTags tag) {
 		String query="INSERT INTO SementicTags (id,label,search,description) VALUES (";
-		query+=tag.getId()+", ";
+		query+=tag.getId()+",";
 		query+=tag.getLabel()+", ";
 		query+=tag.getSearch()+", ";
 		query+=tag.getDescription()+");";
