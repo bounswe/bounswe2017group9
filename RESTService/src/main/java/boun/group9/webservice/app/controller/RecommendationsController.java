6d01a7958ac189ff7829fc6952392f1c7d1ac004
kasimbozdag <bozdag80@yahoo.com>
2017-11-23 03:12:33 +0200

    Adding Recommedations by Semantic Tags

--- RESTService/src/main/java/boun/group9/webservice/app/controller/RecommendationsController.java
+++ RESTService/src/main/java/boun/group9/webservice/app/controller/RecommendationsController.java
@@ -5,12 +5,16 @@ import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -77,8 +81,71 @@ public class RecommendationsController {
 			ex.printStackTrace();
 			return "Not saved.";
 		}
-		Map<Integer, Integer> newMap = new TreeMap(Collections.emptySortedMap());
-		newMap.putAll(recommendations);
+		Map<Integer, Integer> newMap = 
+			     recommendations.entrySet().stream()
+			    .sorted(Entry.<Integer, Integer>comparingByValue().reversed())
+			    .collect(Collectors.toMap(Entry::getKey, Entry::getValue,
+			                              (e1, e2) ->e1, LinkedHashMap::new));
+		//Map<Integer, Integer> newMap = new TreeMap<Integer, Integer>(Collections.emptySortedMap());
+		//newMap.putAll(recommendations);
+		String json = new ObjectMapper().writeValueAsString(newMap);
+		return json;
+	}
+	@RequestMapping(value="recommendations2/{userID}",method=RequestMethod.GET)
+	public String getRecommendations2(@PathVariable(value="userID") int userID) throws JsonParseException, JsonMappingException, IOException  {
+		String query,tag_id;
+		int concert_id,status,score;
+		score=0;
+		Map<Integer, Integer> recommendations=new HashMap<Integer, Integer>();
+		Map<String, Integer> tags=new HashMap<String, Integer>();
+		ArrayList<Integer> userconcerts=new ArrayList<Integer>();
+		ResultSet rs,rs2,rs3;
+		query="SELECT concert_id From Attendees WHERE id="+userID;
+		try {
+			rs = Database.connect(query, Application.MODE_GET);
+			while(rs.next()) {
+				concert_id=rs.getInt("concert_id");
+				userconcerts.add(concert_id);
+				query="SELECT tag_id FROM ConcertTags WHERE concert_id="+concert_id;
+				rs2 = Database.connect(query, Application.MODE_GET);
+				while(rs2.next()) {
+					score=1;
+					tag_id=rs2.getString("tag_id");
+					if(tags.get(tag_id)!=null) {
+						score=tags.get(tag_id)+1;
+					}
+					tags.put(tag_id, score);
+					
+				}
+			}
+			for(String tag:tags.keySet()) {
+				query="SELECT ConcertTags.concert_id As concert_id FROM ConcertTags INNER JOIN Concerts ON Concerts.id=ConcertTags.concert_id WHERE tag_id=\""+tag+"\" AND Concerts.date_time > now();";
+				rs3 = Database.connect(query, Application.MODE_GET);
+				while(rs3.next()) {
+					concert_id=rs3.getInt("concert_id");
+					if(userconcerts.contains(concert_id)) continue;
+					score=tags.get(tag);
+					if(recommendations.get(concert_id)!=null) {
+						score+=recommendations.get(concert_id);
+					}
+					recommendations.put(concert_id,score);
+				}
+			}
+		}catch(SQLException ex) {
+			System.out.println("SQL Exception occured");
+			ex.printStackTrace();
+			return "SQL Error occured.";
+		}catch(NotSavedException ex) {
+			ex.printStackTrace();
+			return "Not saved.";
+		}
+		Map<Integer, Integer> newMap = 
+			     recommendations.entrySet().stream()
+			    .sorted(Entry.<Integer, Integer>comparingByValue().reversed())
+			    .collect(Collectors.toMap(Entry::getKey, Entry::getValue,
+			                              (e1, e2) ->e1, LinkedHashMap::new));
+		//Map<Integer, Integer> newMap = new TreeMap<Integer, Integer>(Collections.emptySortedMap());
+		//newMap.putAll(recommendations);
 		String json = new ObjectMapper().writeValueAsString(newMap);
 		return json;
 	}
