9473babf213fa56025fd4977b9bfb9fb5d75b96a
ffguven <fatih.guven@boun.edu.tr>
2017-11-28 16:10:37 +0300

    target-removed

--- ConcerterBackend/target/classes/static/js/home-page-functions.js
+++
@@ -1,7 +0,0 @@
-    var imageElement = document.getElementsByClassName("card-image");
-    for(var i = 0; i < imageElement.length; i++){
-        imageElement[i].style.height = imageElement.style.width * 0.5;
-        console.log(imageElement.style.width +" " + i);
-    }
-
-
