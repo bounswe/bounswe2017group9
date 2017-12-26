2d643dcdff14046beab6d68b1ea2348d7edcb7d6
Begun <begununal@gmail.com>
2017-12-26 20:39:39 +0300

    Implementations of getFollowers and getFollowings methods

--- RESTService/src/main/java/boun/group9/webservice/app/controller/UserController.java
+++ RESTService/src/main/java/boun/group9/webservice/app/controller/UserController.java
@@ -2,6 +2,7 @@ package boun.group9.webservice.app.controller;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -178,5 +179,16 @@ public class UserController {
 		
 
 	}
+	@RequestMapping(value="followings/{userID}",method= RequestMethod.GET)
+	public static String getFollowings(@PathVariable(value = "userID") int userID){
+		ArrayList<Users> users  = UserChecker.getFollowings(userID);
+		return Application.gson.toJson(users);
+	}
+	@RequestMapping(value="followers/{userID}",method= RequestMethod.GET)
+	public static String getFollowers(@PathVariable(value = "userID") int userID){
+		ArrayList<Users> users  = UserChecker.getFollowers(userID);
+		return Application.gson.toJson(users);
+	}
+
 
 }
--- RESTService/src/main/java/boun/group9/webservice/helper/UserChecker.java
+++ RESTService/src/main/java/boun/group9/webservice/helper/UserChecker.java
@@ -1,6 +1,7 @@
 package boun.group9.webservice.helper;
 
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import boun.group9.webservice.app.Application;
 import boun.group9.webservice.app.data.Users;
@@ -80,6 +81,41 @@ public class UserChecker {
 	public static String unFollowed(int userID){
 		String query = "Update users SET followers = followers - 1 WHERE users.id = "+ userID +  ";";
 		return query;
+	}
+	public static ArrayList<Users> getFollowings(int userID){
+		String query = "SELECT following_id FROM Relations WHERE follower_id = " + userID + ";";
+		ResultSet rs;
+		ArrayList<Users> users = new ArrayList<Users>();
+		int f_id;
+		try{
+			rs = Database.connect(query, Application.MODE_GET);
+			while(rs.next()){
+				f_id = rs.getInt("following_id");
+				users.add(getUser(f_id));
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}
+		return users;
+	}
+
+	public static ArrayList<Users> getFollowers(int userID){
+		String query = "SELECT follower_id FROM Relations WHERE following_id = " + userID + ";";
+		ResultSet rs;
+		ArrayList<Users> users = new ArrayList<Users>();
+		int f_id;
+		try{
+			rs = Database.connect(query, Application.MODE_GET);
+			while(rs.next()){
+				f_id = rs.getInt("follower_id");
+				users.add(getUser(f_id));
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}
+		return users;
 	}
 
 
