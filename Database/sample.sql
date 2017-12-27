f736ba7c793e5dab44ffd39ee21bf84bdd478eb4
Begun <begununal@gmail.com>
2017-12-28 00:17:25 +0300

    Comments_categories table in db was changed

--- Database/sample.sql
+++ Database/sample.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: concerter_db
 -- ------------------------------------------------------
--- Server version	5.7.20-0ubuntu0.17.10.1
+-- Server version	5.7.20-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -113,7 +113,7 @@ CREATE TABLE `Comments_categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `category` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -122,7 +122,7 @@ CREATE TABLE `Comments_categories` (
 
 LOCK TABLES `Comments_categories` WRITE;
 /*!40000 ALTER TABLE `Comments_categories` DISABLE KEYS */;
-INSERT INTO `Comments_categories` VALUES (1,'Costume'),(2,'Music'),(3,'Place'),(4,'Foods');
+INSERT INTO `Comments_categories` VALUES (1,'Costume'),(2,'Music'),(3,'Place'),(4,'Price'),(5,'Transportation');
 /*!40000 ALTER TABLE `Comments_categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -421,4 +421,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-12-27 23:52:27
+-- Dump completed on 2017-12-28  0:14:54
